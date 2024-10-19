8c43f4b764ef1298346732ab5c23bf6b39c1746e
MilanDanilovic <mdanilovic06@gmail.com>
2024-10-19 03:41:16 +0200

    real image preview

--- components/Places/PlaceForm.js
+++ components/Places/PlaceForm.js
@@ -1,13 +1,30 @@
 import { ScrollView, StyleSheet, Text, TextInput, View } from "react-native";
-import { useState } from "react";
+import { useState, useCallback } from "react";
 import { Colors } from "../../constants/colors";
 import ImagePicker from "./ImagePicker";
 import LocationPicker from "./LocationPicker";
+import Button from "../UI/Button";
 
 function PlaceForm() {
   const [title, setTitle] = useState("");
   function changeTitleHandler(text) {
     setTitle(text);
+  }
+  const [selectedImage, setSelectedImage] = useState();
+  const [pickedLocation, setPickedLocation] = useState();
+
+  function takeImageHandler(imageUri) {
+    setSelectedImage(imageUri);
+  }
+
+  const pickLocationHandler = useCallback((location) => {
+    setPickedLocation(location);
+  }, []);
+
+  function savePlaceHandler() {
+    console.log("title", title);
+    console.log("selectedImage", selectedImage);
+    console.log("pickedLocation", pickedLocation);
   }
 
   return (
@@ -20,8 +37,9 @@ function PlaceForm() {
           value={title}
         />
       </View>
-      <ImagePicker />
-      <LocationPicker />
+      <ImagePicker onTakeImage={takeImageHandler} />
+      <LocationPicker onPickLocation={pickLocationHandler} />
+      <Button onPress={savePlaceHandler}>Add Place</Button>
     </ScrollView>
   );
 }
